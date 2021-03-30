5a0891f95c89ba4b03a0c82f457a3c5e583587a5
monisio <monisio86@gmail.com>
2021-03-30 20:29:55 +0200

    Adding Final exam exercise.

--- JavaTrainingPath/JavaFundamentalsCourse/FinalExamPreparation/E02EmojiDetector.java
+++ JavaTrainingPath/JavaFundamentalsCourse/FinalExamPreparation/E02EmojiDetector.java
@@ -1,5 +1,6 @@
 package JavaFundamentalsCourse.FinalExamPreparation;
 
+import java.math.BigInteger;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,50 +12,47 @@ public class E02EmojiDetector {
 
         String input = scanner.nextLine();
 
-        long coolThreshold = getCoolThreshold(input);
+        BigInteger coolThreshold = getCoolThreshold(input);
 
 
         Pattern findEmoji = Pattern.compile("([:|*]{2})(?<letters>[A-Z]{1}[a-z]{2,})\\1");
         Matcher matcher = findEmoji.matcher(input);
 
-        Map<String, Integer> emojis = new LinkedHashMap<>();
-
-        while (matcher.find()) {
-            String emoji = matcher.group();
-
-            int sum = matcher.group("letters").chars().sum();
-
-            emojis.putIfAbsent(emoji, sum);
-        }
-
         List <String> coolOnes = new ArrayList<>();
 
-        emojis.forEach((key, value) -> {
-            if (value >= coolThreshold) {
-                coolOnes.add(key);
+        int counter = 0;
+        while (matcher.find()) {
+            counter++;
+            int sum = matcher.group("letters").chars().sum();
+
+            if(coolThreshold.compareTo(BigInteger.valueOf(sum))<0) {
+                coolOnes.add(matcher.group());
             }
-        });
+
+        }
+
+
+        System.out.println("Cool threshold: " + coolThreshold);
+        System.out.println(counter + " emojis found in the text. The cool ones are:");
 
         if (!coolOnes.isEmpty()) {
 
-            System.out.println("Cool threshold: " + coolThreshold);
-            System.out.println(emojis.size() + " emojis found in the text. The cool ones are:");
             coolOnes.forEach(System.out::println);
 
         }
 
+
+
     }
 
-    private static long getCoolThreshold(String input) {
-        long result = 1;
+    private static BigInteger getCoolThreshold(String input) {
+      //  long result = 1;
+        BigInteger result = new BigInteger("1");
 
-        Pattern thresholdPattern = Pattern.compile("[\\d]");
-
+        Pattern thresholdPattern = Pattern.compile("\\d");
         Matcher matcher = thresholdPattern.matcher(input);
-
         while (matcher.find()) {
-            long multiplier = Integer.parseInt(matcher.group());
-            result *= multiplier;
+         result = result.multiply(BigInteger.valueOf(Integer.parseInt(matcher.group())));
 
         }
 
