8dfc7debdc00fe80a808d80f2de1ed60c284df00
monisio <monisio86@gmail.com>
2021-04-07 17:38:54 +0200

    Adding Functional Programming lab problem 01.

--- JavaTrainingPath/JavaAdvancedCourse/FunctionalProgramming/E01.java
+++ JavaTrainingPath/JavaAdvancedCourse/FunctionalProgramming/E01.java
@@ -1,4 +1,25 @@
 package JavaAdvancedCourse.FunctionalProgramming;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class E01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> input = Arrays.stream(scanner.nextLine().split(", ")).map(Integer::parseInt)
+                .filter(e -> e % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(input.toString().replaceAll("[\\[\\]]", ""));
+
+        Collections.sort(input);
+
+        System.out.print(input.toString().replaceAll("[\\[\\]]", ""));
+
+
+    }
 }
