0d1cb557f8d06f966b1bec590b3ce7a2d9a363a0
monisio <monisio86@gmail.com>
2021-04-08 20:38:42 +0200

    Exercise E04 added in Functional programming folder.

---
+++ JavaTrainingPath/JavaAdvancedCourse/FunctionalProgramming/Exercise/E04AppliedArithmetics.java
@@ -0,0 +1,50 @@
+package JavaAdvancedCourse.FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class E04AppliedArithmetics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> inputNumbers = Arrays.stream(scanner.nextLine()
+                .split("\\s"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("end")){
+
+                 inputNumbers = inputNumbers.stream().map(getFunction(command)).collect(Collectors.toList());
+
+
+            command= scanner.nextLine();
+        }
+
+
+        inputNumbers.forEach(e-> System.out.print(e+" "));
+
+    }
+
+    public static Function<Integer , Integer> getFunction(String factor){
+
+        switch (factor){
+            case "add":
+                return n -> n+1;
+            case "multiply":
+                return n -> n*2;
+            case "subtract":
+                return n -> n-1;
+
+            default:
+                throw new IllegalStateException("Unexpected operation: " + factor);
+        }
+
+
+    }
+}
