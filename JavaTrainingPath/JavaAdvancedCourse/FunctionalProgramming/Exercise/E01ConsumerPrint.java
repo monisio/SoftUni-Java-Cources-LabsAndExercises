28c81c6317fd48534134bceffe1e927869462641
monisio <monisio86@gmail.com>
2021-04-08 19:30:24 +0200

    Finish E05.

---
+++ JavaTrainingPath/JavaAdvancedCourse/FunctionalProgramming/Exercise/E01ConsumerPrint.java
@@ -0,0 +1,25 @@
+package JavaAdvancedCourse.FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class E01ConsumerPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        List<String> input = Arrays.stream(scanner.nextLine().split("\\s")).collect(Collectors.toList());
+
+        Consumer<String> printer = System.out::println;
+
+
+        input.forEach(printer);
+
+
+
+
+    }
+}
