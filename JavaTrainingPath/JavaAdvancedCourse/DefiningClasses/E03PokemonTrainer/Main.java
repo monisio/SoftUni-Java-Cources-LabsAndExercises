c663ee7371773529bac779ebe2ddfc94b2074dd2
monisio <monisio86@gmail.com>
2021-04-18 16:35:49 +0200

    Defining classes Exercise 3 corrected.

--- JavaTrainingPath/JavaAdvancedCourse/DefiningClasses/E03PokemonTrainer/Main.java
+++ JavaTrainingPath/JavaAdvancedCourse/DefiningClasses/E03PokemonTrainer/Main.java
@@ -1,12 +1,54 @@
 package JavaAdvancedCourse.DefiningClasses.E03PokemonTrainer;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+       Map<String, Trainer> trainerMap = new LinkedHashMap<>();
 
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("Tournament")) {
+            String[] tokens = input.split("\\s+");
+            String trainerName = tokens[0];
+            String pokemonName = tokens[1];
+            String pokemonElement = tokens[2];
+            int pokemonHealth = Integer.parseInt(tokens[3]);
+
+            Trainer current = trainerMap.get(trainerName);
+
+            if (current == null) {
+                trainerMap.put(trainerName, new Trainer(trainerName));
+                current = trainerMap.get(trainerName);
+            }
+
+            current.addPokemon(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+
+            input = scanner.nextLine();
+        }
+
+        input = scanner.nextLine();
+
+
+        while (!input.equals("End")) {
+
+            for (Trainer trainer : trainerMap.values()) {
+
+                trainer.checkElement(input);
+            }
+
+
+            input = scanner.nextLine();
+        }
+
+        trainerMap.entrySet().stream()
+                .sorted((t1, t2) -> Integer.compare(t2.getValue().getBadges(), t1.getValue().getBadges()))
+                .forEach(t -> System.out.println(t.getKey()+" "+ t.getValue().getBadges() +" "+ t.getValue().getPokemonAmount()));
 
     }
 }
--- JavaTrainingPath/JavaAdvancedCourse/DefiningClasses/E03PokemonTrainer/Trainer.java
+++ JavaTrainingPath/JavaAdvancedCourse/DefiningClasses/E03PokemonTrainer/Trainer.java
@@ -2,6 +2,7 @@ package JavaAdvancedCourse.DefiningClasses.E03PokemonTrainer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Trainer {
 
@@ -17,36 +18,33 @@ public class Trainer {
     }
 
 
-    public void addPokemon(Pokemon newPokemon){
-        this.pokemonCollection.add(newPokemon);
-    }
-
-
-    public int getPokemonAmount(){
-        return pokemonCollection.size();
-    }
 
     public int getBadges(){
         return this.badges;
     }
 
+    public int getPokemonAmount(){
+        return pokemonCollection.size();
+    }
+
+    public void addPokemon(Pokemon newPokemon){
+        this.pokemonCollection.add(newPokemon);
+    }
+
     public void checkElement(String element){
-        long count = this.pokemonCollection.stream().filter(pokemon -> pokemon.getElement().equals(element)).count();
-        if (count == 0){
-            hitCollection();
-        }else{
-            this.badges++;
-        }
+       if(!this.pokemonCollection.isEmpty()){
+           long count = this.pokemonCollection.stream().filter(pokemon -> pokemon.getElement().equals(element)).count();
+
+           if (count != 0){
+                   this.badges+=1;
+           }else{
+               pokemonCollection.forEach(e->e.setHealth(e.getHealth()-10));
+              this.pokemonCollection = pokemonCollection.stream().filter(e->e.getHealth()>0).collect(Collectors.toList());
+           }
+
+       }
     }
 
-    private void hitCollection(){
-        for (Pokemon pokemon : pokemonCollection) {
-            pokemon.setHealth(pokemon.getHealth() - 10);
-            if (pokemon.getHealth()<=0){
-                pokemonCollection.remove(pokemon);
-            }
 
-        }
 
-    }
 }
