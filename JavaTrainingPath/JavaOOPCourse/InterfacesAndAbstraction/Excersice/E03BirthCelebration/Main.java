7ac5f3806db21dd846483aaa8324e6e5e49451d6
monisio <monisio86@gmail.com>
2021-06-06 14:04:08 +0200

    Interfaces lab added.

---
+++ JavaTrainingPath/JavaOOPCourse/InterfacesAndAbstraction/Excersice/E03BirthCelebration/Main.java
@@ -0,0 +1,52 @@
+package JavaOOPCourse.InterfacesAndAbstraction.Excersice.E03BirthCelebration;
+
+import JavaOOPCourse.InterfacesAndAbstraction.Excersice.E01DefineAnInterfacePerson.Citizen;
+import JavaOOPCourse.InterfacesAndAbstraction.Excersice.E02MultipleImplementation.Birthable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        String input = scanner.nextLine();
+
+        List<Birthable> birthableList = new ArrayList<>();
+
+        while (!input.equals("End")) {
+            String[] tokens = input.split("\\s+");
+
+
+            switch (tokens[0]) {
+
+                case "Citizen":
+                    birthableList.add(new Citizen(tokens[1], Integer.parseInt(tokens[2]), tokens[3], tokens[4]));
+                    break;
+
+                case "Pet":
+                    birthableList.add(new Pet(tokens[1], tokens[2]));
+
+
+            }
+
+
+            input = scanner.nextLine();
+        }
+
+        String searchedYear = scanner.nextLine();
+
+        for (Birthable birthable : birthableList) {
+            String birthDate = birthable.getBirthDate();
+
+            if (birthDate.endsWith(searchedYear)){
+                System.out.println(birthDate);
+            }
+
+        }
+
+
+    }
+}
